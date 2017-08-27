6ab7b675d8c9a307c47d1a2d04364d00549be6c1
Kris Price <kris@krisprice.nz>
2017-08-26 17:25:16 -0700

    Modified

--- aggip/src/main.rs
+++ aggip/src/main.rs
@@ -35,14 +35,76 @@ fn main() {
     print_ipnet_vec(&Ipv4Net::aggregate(&ipv4nets));
     print_ipnet_vec(&Ipv6Net::aggregate(&ipv6nets));
 
-    //let ip = ipv4nets[2];
-    //println!("{:?}", ip.subnets(28));
-    
-    let a = Ipv6Addr::from_str("fd00::1").unwrap();
-    let u = Emu128 { hi: 0xfd00_0000_0000_0000, lo: 1 };
-    let a2: Ipv6Addr = u.into();
+    // Test subnets iterator
+    /*let v: Vec<Ipv4Net> = Ipv4Net::from_str("10.1.1.0/24").unwrap().new_subnets(26).into_iter().collect();
+    println!("{:?}", v);
 
-    assert_eq!(a, a2);
-    assert_eq!(u, a.into());
-    assert_eq!(u, Emu128::from(a));
+    let i = Ipv4Net::from_str("10.1.1.0/24").unwrap().new_subnets(8);
+    for ip in i {
+        println!("{}", ip);
+    }
+    let i = Ipv6Net::from_str("fd00::/16").unwrap().new_subnets(18);
+    for ip in i {
+        println!("{}", ip);
+    }
+    let h = Ipv4Net::from_str("10.1.1.0/24").unwrap().hosts();
+    for ip in h {
+        println!("{}", ip);
+    }
+
+    let h = Ipv6Net::from_str("fd00::/125").unwrap().hosts();
+    for ip in h {
+        println!("{}", ip);
+    }*/
+
+
+    // TODO:
+    // * impl Range for Emu128 as a test
+    // * Create a custom IpRange trait for requires IpAdd trait
+    // * Can the Range and step_by make sense for subnets()?
+    // would be good to have Add and Step impl for IpAddr so can use Range
+    // Range and Step are not stable so stick with custom iterator until
+    // they are
+
+    use std::ops::Range;
+
+    let r = 1..5;
+    println!("{} {}", r.start, r.end);
+
+    /*let mut r = Range {
+        start: Ipv4Addr::from_str("10.1.1.1").unwrap(),
+        end: Ipv4Addr::from_str("10.1.1.20").unwrap(),
+    };
+
+    for i in r {
+        println!("{}", i);
+    }*/
+    
+    #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+    struct NewType(u32);
+
+    // I get the 
+    impl std::ops::Add for NewType {
+        type Output = NewType;
+        fn add(self, other: NewType) -> NewType {
+            NewType(self.0 + other.0)
+        }
+    }
+
+    // This is marked nightly-only and so generates the "error: use of
+    // unstable library feature 'step_trait': likely to be replaced by
+    // finer-grained traits (see issue #42168)." Yet I can use other
+    // Ranges (e.g. just doing 1..5) that also must've implemented
+    // this trait. How come those are allowed?
+    //impl std::iter::Step for NewType {
+        // ...
+    //}
+
+    let a = NewType(10);
+    let b = NewType(20);
+
+    for i in a..b {
+        println!("{:?}", i);
+    }
+
 }
